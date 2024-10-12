146257753b3a9306fd50cf4d3a583593a6f5dafb
Tibor Leupold <tibor@lpld.io>
2024-10-12 07:35:30 -0700

    Element to log it's child elements

--- maybe-script.js
+++ maybe-script.js
@@ -1,7 +1,23 @@
 class MaybeScript extends HTMLElement {
     constructor() {
-        super()
+        // Super constructor returns a reference to the element itself.
+        self = super()
         console.log("Custom element constructed")
+    }
+
+    connectedCallback() {
+        console.log("Custom element connected")
+        // When this element itself (the parent) is added to the DOM, the children are not constructed yet.
+        // Thus, we need to wait for the DOM to be loaded.
+        document.addEventListener("DOMContentLoaded", this.handleDOMLoaded)
+    }
+
+    handleDOMLoaded(event) {
+        console.log("Element responds to DOM is loaded.", self)
+        // Hide the child elements
+        for (const child of self.children) {
+            console.log(child.tagName)
+        }
     }
 }
 
