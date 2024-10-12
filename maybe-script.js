46939d3447674eeeabc9e438cc70e4d993aa5228
Tibor Leupold <tibor@lpld.io>
2024-10-12 09:45:15 -0700

    Show element after delay

--- maybe-script.js
+++ maybe-script.js
@@ -8,12 +8,27 @@ class MaybeScript extends HTMLElement {
     connectedCallback() {
         console.log("Custom element connected", this)
         this.hide()
+        this.showAfterDOMandDelay()
     }
 
     hide() {
         console.log("Hiding", this)
         this.setAttribute("hidden", "")
     }
+
+    show() {
+        console.log("Showing", this)
+        this.removeAttribute("hidden")
+    }
+
+    showAfterDelay()  {
+        setTimeout(() => this.show(), 2000)
+    }
+
+    showAfterDOMandDelay() {
+        document.addEventListener("DOMContentLoaded", () => this.showAfterDelay())
+    }
+
 }
 
 customElements.define("maybe-script", MaybeScript)
