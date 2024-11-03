d51bab68bb16b212dd73904484042573a50a8010
Tibor Leupold <tibor@lpld.io>
2024-11-03 07:41:56 -0800

    Integrate element register and script status reporting

--- maybe-script.js
+++ maybe-script.js
@@ -1,95 +1,121 @@
-(() => {
-    // This runs where the script is included.
-    // So at the top of the document.
-    console.log("maybe-script.js running")
+// This runs where the script is included.
+// So at the top of the document.
+console.log("maybe-script.js running")
 
 
-    class Register {
-        constructor() {
-            this.map: new Map()
-        }
-
-        add(maybeScript) {
-            // Get absolute URL from the `src` attribute.
-            // This is needed because the performance entries use the absolute URL.
-            const src = maybeScript.getAttribute("src")
-            if (!src) return
-
-            const documentURL = new URL(document.URL)
-            const srcURL = new URL(src, documentURL.origin)
-            const absoluteSrcHref = srcURL.href
-
-            this.map[absoluteSrcHref] = maybeScript
-        }
-    }
-
-    // Create register
-    if (window.maybeScript === undefined) {
-        window.maybeScript = new Register()
-    }
-
-    class MaybeScript extends HTMLElement {
-        constructor() {
-            // Super constructor returns a reference to the element itself.
-            super()
-            console.log("Custom element constructed", this)
-        }
-
-        connectedCallback() {
-            console.log("Custom element connected", this)
-            this.hide()
-
-
-            // How do we make sure this runs after the event listenr that adds the statuses?
-            // I guess we can use a custom event that is fired by  that status tracker.
-            window.addEventListener("load", () => {
-                console.log("Handling for elemnt after load", this)
-                const scriptState = this.getScriptStatus()
-                console.log(scriptState)
-                if (!MaybeScript.responseOk(scriptState)) {
-                    this.show()
-                }
-            })
-        }
-
-        hide() {
-            console.log("Hiding", this)
-            this.setAttribute("hidden", "")
-        }
-
-        show() {
-            console.log("Showing", this)
-            this.removeAttribute("hidden")
-        }
-
-        static responseOk(statusCode) {
-            return statusCode >= 200 && statusCode <400
-        }
-
-        getScriptStatus() {
-            return window.maybeScript.states[this.srcURL]
-        }
-    }
+function main() {
+    createRegister()
 
     customElements.define("maybe-script", MaybeScript)
 
-    document.addEventListener("DOMContentLoaded", () => {
-        console.log("DOM Content Loaded")
-    })
+    setUpScriptStateReporting()
+}
 
 
+function createRegister() {
+    if (window.maybeScript === undefined) {
+        window.maybeScript = new Register()
+    }
+}
+
+
+function responseStatusOk(statusCode) {
+    return statusCode >= 200 && statusCode <400
+}
+
+
+function setUpScriptStateReporting() {
     window.addEventListener("load", () => {
-        console.log("Everything Loaded")
-
-        console.log("Setting up script loading checks")
+        console.log("Setting up reporting of script loading states")
         const performanceObserver = new PerformanceObserver((list) => {
             list.getEntries().forEach((entry) => {
                 if (entry.initiatorType === "script") {
-                    window.maybeScript.states[entry.name] = entry.responseStatus
+                    window.maybeScript.reportScriptState(entry.name, entry.responseStatus)
                 }
             })
         })
+        // Buffered makes sure we get historic entires
         performanceObserver.observe({type: "resource", buffered: true})
     })
-})();
+}
+
+
+class Register {
+    constructor() {
+        this.map = new Map()
+    }
+
+    addCustomElement(maybeScript) {
+        // Get absolute URL from the `src` attribute.
+        // This is needed because the performance entries use the absolute URL.
+        const src = maybeScript.getAttribute("src")
+        if (!src) return
+
+        const documentURL = new URL(document.URL)
+        const srcURL = new URL(src, documentURL.origin)
+        const absoluteSrcHref = srcURL.href
+
+
+        let customElements = this.map.get(absoluteSrcHref)
+        if (customElements === undefined) {
+            customElements = []
+        }
+        customElements.push(maybeScript)
+        this.map.set(absoluteSrcHref, customElements)
+    }
+
+    reportScriptState(scriptURL, status) {
+        console.log("Reporting script status", scriptURL, status)
+
+        const customElements = this.map.get(scriptURL)
+        if (customElements === undefined) return
+
+        customElements.forEach((ce) => ce.updateForScriptStatus(status))
+    }
+}
+
+
+class MaybeScript extends HTMLElement {
+    constructor() {
+        // Super constructor returns a reference to the element itself.
+        super()
+        console.log("Custom element constructed", this)
+    }
+
+    connectedCallback() {
+        console.log("Custom element connected", this)
+
+        if (window.maybeScript === undefined) {
+            console.error("No maybeScript register found.")
+            return
+        }
+
+        this.hide()
+
+        window.maybeScript.addCustomElement(this)
+    }
+
+    updateForScriptStatus(status) {
+        console.log("Updating custom element for script status", this, status)
+
+        // If the script loaded ok, we don't do anything
+        if (responseStatusOk(status)) return
+
+
+        this.show()
+    }
+
+    hide() {
+        console.log("Hiding", this)
+        this.setAttribute("hidden", "")
+    }
+
+    show() {
+        console.log("Showing", this)
+        this.removeAttribute("hidden")
+    }
+}
+
+
+(() => {main()})();
 
