ed0db37b45918e62e8099c9260dba65d275177ec
Tibor Leupold <tibor@lpld.io>
2024-11-02 18:22:21 -0700

    Clarify console output

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function main() {
     const body = document.querySelector("body")
     if (!body) return
 
-    console.log("Hello")
+    console.log("Hello from main() function")
 }
 
-console.log("main.js running")
+console.log("main.js script running")
 
 document.addEventListener("DOMContentLoaded", main)
