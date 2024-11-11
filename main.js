59d0de24fdab0b8ec7932f2785cc347d083ce31f
Tibor Leupold <tibor@lpld.io>
2024-11-11 06:16:38 -0800

    Finish setup of example disclosure

--- main.js
+++ main.js
@@ -3,24 +3,64 @@ function main() {
     if (!body) return
 
     console.log("Hello from main() function")
-    Component.init()
     Disclosure.init()
 }
 
 
 class Component {
     static init() {
-        console.debug("Initializing")
-        const elems = document.querySelectorAll(this.selector)
-        console.debug(elems)
-        elems.forEach((elem) => {console.debug(elem)})
+        console.debug("Initializing", this)
+        const elems = document.querySelectorAll(this.selector())
+
+        elems.forEach((elem) => {
+            new this(elem)
+        })
+    }
+
+    static selector () {
+        return undefined
     }
 }
 
 
 class Disclosure extends Component {
-    static selector = "[data-disclosure]"
+    static selector () {return "[data-disclosure]"}
 
+    constructor(elem) {
+        super()
+
+        this.button = elem
+        const contentId = this.button.dataset.disclosureToggles
+        const contentSelector = `[data-disclosure-content=${contentId}]`
+
+        this.content = document.querySelector(contentSelector)
+        if (!this.content) return
+
+        this.button.addEventListener("click", () => {this.toggle()})
+        this.hide()
+    }
+
+    toggle() {
+        if (this.isHidden()) {
+            this.show()
+        } else {
+            this.hide()
+        }
+    }
+
+    hide () {
+        console.debug("Hiding", this.content)
+        this.content.setAttribute("hidden", "")
+    }
+
+    show () {
+        console.debug("Showing", this.content)
+        this.content.removeAttribute("hidden")
+    }
+
+    isHidden () {
+        return this.content.getAttribute("hidden") !== null
+    }
 }
 
 console.log("main.js script running")
