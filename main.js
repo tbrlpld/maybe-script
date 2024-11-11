ad18037ae66b2a80a41fb091a5ef83a374561670
Tibor Leupold <tibor@lpld.io>
2024-11-10 20:05:15 -0800

    Start adding some example use cases

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ function main() {
     if (!body) return
 
     console.log("Hello from main() function")
+    Component.init()
+    Disclosure.init()
+}
+
+
+class Component {
+    static init() {
+        console.debug("Initializing")
+        const elems = document.querySelectorAll(this.selector)
+        console.debug(elems)
+        elems.forEach((elem) => {console.debug(elem)})
+    }
+}
+
+
+class Disclosure extends Component {
+    static selector = "[data-disclosure]"
+
 }
 
 console.log("main.js script running")
